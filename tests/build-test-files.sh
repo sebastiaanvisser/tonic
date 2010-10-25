10b20f86faf5f5e428569c09ae8ed76f479813b8
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-10-25 23:48:45 +0200

    Script for building big xml files.

---
+++ tests/build-test-files.sh
@@ -0,0 +1,14 @@
+curl http://www.w3.org/TR/REC-xml/ > REC-xml.html
+
+cat REC-xml.html >> 2x.html
+cat REC-xml.html >> 2x.html
+cat 2x.html >> 4x.html
+cat 2x.html >> 4x.html
+cat 4x.html >> 8x.html
+cat 4x.html >> 8x.html
+cat 8x.html >> 16x.html
+cat 8x.html >> 16x.html
+cat 16x.html >> 32x.html
+cat 16x.html >> 32x.html
+cat 32x.html >> 64x.html
+cat 32x.html >> 64x.html
