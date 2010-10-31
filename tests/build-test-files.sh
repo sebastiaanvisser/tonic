c7848368c45f9c99e4ce7b52e2e05557d6ceeaf5
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-10-31 20:39:49 +0100

    Updated test file builder script.

--- tests/build-test-files.sh
+++ tests/build-test-files.sh
@@ -1,14 +1,17 @@
-curl http://www.w3.org/TR/REC-xml/ > REC-xml.html
+curl http://www.w3.org/TR/REC-xml/ > files/REC-xml.html
 
-cat REC-xml.html >> 2x.html
-cat REC-xml.html >> 2x.html
-cat 2x.html >> 4x.html
-cat 2x.html >> 4x.html
-cat 4x.html >> 8x.html
-cat 4x.html >> 8x.html
-cat 8x.html >> 16x.html
-cat 8x.html >> 16x.html
-cat 16x.html >> 32x.html
-cat 16x.html >> 32x.html
-cat 32x.html >> 64x.html
-cat 32x.html >> 64x.html
+tidy --asxml files/REC-xml.html > files/REC-xml.xml
+
+cat files/REC-xml.xml >> files/2x.xml
+cat files/REC-xml.xml >> files/2x.xml
+cat files/2x.xml >> files/4x.xml
+cat files/2x.xml >> files/4x.xml
+cat files/4x.xml >> files/8x.xml
+cat files/4x.xml >> files/8x.xml
+cat files/8x.xml >> files/16x.xml
+cat files/8x.xml >> files/16x.xml
+cat files/16x.xml >> files/32x.xml
+cat files/16x.xml >> files/32x.xml
+cat files/32x.xml >> files/64x.xml
+cat files/32x.xml >> files/64x.xml
+
