b3c26cfa65158a7faa41a9d214f85cc2ab4093c6
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-12-06 21:47:02 +0100

    Minor updates to test file.

--- src/Test.hs
+++ src/Test.hs
@@ -7,7 +7,7 @@ import Control.Arrow
 import Control.Category
 import Prelude hiding (id, (.), elem)
 import Xml.Tonic.Arrow
-import Xml.Tonic.Types (Xml(..), Node(..))
+import Xml.Tonic.Types (Xml, Node(..))
 import qualified Data.Text.Lazy    as T
 import qualified Data.Text.Lazy.IO as T
 import qualified Xml.Tonic.Parse as Parse
@@ -15,10 +15,10 @@ import qualified Xml.Tonic.Print as Print
 
 main :: IO ()
 main =
-  do f <- T.readFile "tests/files/4x.xml"
-     mapM_ T.putStrLn (transform myTransformation f)
+  do f <- T.readFile "tests/files/64x.xml"
+--      mapM_ T.putStrLn (transform myTransformation f)
+     print $ length (transform myTransformation f)
   where
     transform tr = runListArrow tr . Parse.xml
-
-    myTransformation = {-# SCC "name" #-} attr "href" . deep (elem "a") . isElem . unlist
+    myTransformation = deep (elem "a") . isElem . unlist
 
